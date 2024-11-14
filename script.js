272fe57e3e1471b60a770d951aa9929dfc787656
Alifia Shasa <145659930+Alifia-30@users.noreply.github.com>
2024-11-15 00:51:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     const menu = document.getElementById("menu");
     menu.classList.toggle("show");
 }
+window.addEventListener('scroll', function() { //untuk mendeteksi ketika melakukan scroll.
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) { // Ketika scroll lebih dari 50px
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
